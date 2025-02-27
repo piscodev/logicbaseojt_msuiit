707542f1e4279c36828f5d0587ae1b6b102fc591
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-02-27 11:28:40 +0800

    Fixed build issues

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React from "react";
 import { Breadcrumb, Layout, Tabs, ConfigProvider,// Col, Row,
    Card } from "antd";
 // import TransactionForm from "./components/TransactionForm";
@@ -10,13 +10,8 @@ import DataTable from "../components/DataTableTest";
 import Nav from "../components/NavigationBar";
 const { Content, Footer } = Layout;
 const { TabPane } = Tabs;
-const items = new Array(3).fill(null).map((_, index) => ({
-  key: String(index + 1),
-  label: `nav ${index + 1}`,
-}));
 
 export default function Home() {
-  const [tableMode, setTableMode] = useState<string | number>('Map');
   return (
     <ConfigProvider>
       <Layout>
@@ -30,10 +25,7 @@ export default function Home() {
               { title: "Current Page" },
             ]}
           />
-          
           <Card title="Transactions Log">
-            {/* <Segmented options={['Daily', 'Per Cashier']} value={tableMode} onChange={setTableMode} />
-            {tableMode==='Daily'?(<TestTable />):(<DataTable/>)} */}
             <Tabs defaultActiveKey="daily">
               <TabPane tab="Daily" key="daily">
                 <TestTable />
