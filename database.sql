601c66158a5bdae76acb8a2756d21b07bd923c99
RMV-Coder <70832774+RMV-Coder@users.noreply.github.com>
2025-03-20 10:59:02 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -109,6 +109,7 @@ CREATE TABLE IF NOT EXISTS `transaction_detail` (
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `users_notification_subscriptions` (
+  `notification_subscription_id` INT PRIMARY KEY AUTO_INCREMENT,
   `user_id` INT UNSIGNED NOT NULL,
   `auth` VARCHAR(255) NOT NULL,
   `data` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`data`)),
